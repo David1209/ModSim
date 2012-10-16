12839844e08567e9d00628b175825ab2b3b7157b
David van Erkelens <dve@live.nl>
2012-10-16 09:47:54 +0200

    Started working on the assignment

--- 4/main.c
+++ 4/main.c
@@ -1,13 +1,28 @@
 /*
- *  Assignment 4 ModSim
- *  By David van Erkelens & Victor den Haan
+ *  Assignment 4 - Modelating & Simulating Course
+ *  By David van Erkelens <David.vanErkelens@student.uva.nl>
+ *  And Victor den Haan <Victor.denHaan@student.uva.nl>
+ *
+ *  Department of Computer Science
+ *  University of Amsterdam
+ *  October 2012
+ *
+ *  This program describes two cellular automata: Forest Fire Propagation and
+ *  The Spread of Malaria/Predator-Prey Model. The first will be implemented
+ *  anyway, and one (or two, if there is enough time) of the last two.
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ * TODO FOR FOREST FIRE:
+ * 1. Implement the grid (multi dimensional array)
+ * 2.
+ */
+
 int main()
 {
-    printf("My first C program using VIM!\n");
+
     return 0;
 }
